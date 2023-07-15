38be4831887b6165e0ecaae12afbeb02a38177ea
Daksh <dakshkaul7790@gmail.com>
2023-07-15 22:52:34 +0530

    added sounds and added splicing

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import sys
 import os
 from playsound import playsound
 
+
 # Global Key Pressed State Variable 
 key_pressed = False
-sound_directory = '/path/to/directory'
+
+sound_directory = '/Documents/CreamSwitchesRyan'
 file_names = os.listdir(sound_directory)
 
-def on_press(key):
 
+def on_press(key):
     global key_pressed
 
     try:
@@ -23,8 +25,8 @@ def on_press(key):
             key_pressed = True
             print(f'Pressed: {key}')
 
-def on_release(key):
 
+def on_release(key):
     global key_pressed
 
     key_pressed = False
@@ -33,14 +35,15 @@ def on_release(key):
         # Stop listener
         return False
 
+
 def check_for_correct_sound(key):
 
     global sound_directory, files_names
 
     for file_name in file_names:
 
-        if key.toupper() == file_name:
-            playsound(f'{sound_directory}/{file_name}.mp3')
+        if key.char.toupper() or key.toupper() == file_name[:-4]:
+            playsound(f'{sound_directory}/{key.toupper()}.mp3')
 
 listener = keyboard.Listener(on_press=on_press, on_release=on_release)
 
