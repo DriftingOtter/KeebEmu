88946c75f7a3331ed2144866367f6396acdd83b2
Daksh <dakshkaul7790@gmail.com>
2023-07-15 21:03:56 +0530

    added sound play upon key press

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from pynput import keyboard
+import sys
+import os
+from playsound import playsound
 
 # Global Key Pressed State Variable 
 key_pressed = False
+sound_directory = '/path/to/directory'
+file_names = os.listdir(sound_directory)
 
 def on_press(key):
 
@@ -12,6 +17,7 @@ def on_press(key):
             key_pressed = True
             print(f'Pressed: {key.char}')
 
+    # For Special Chars
     except AttributeError:
         if not key_pressed:
             key_pressed = True
@@ -26,6 +32,15 @@ def on_release(key):
     if key == keyboard.Key.esc:
         # Stop listener
         return False
+
+def check_for_correct_sound(key):
+
+    global sound_directory, files_names
+
+    for file_name in file_names:
+
+        if key.toupper() == file_name:
+            playsound(f'{sound_directory}/{file_name}.mp3')
 
 listener = keyboard.Listener(on_press=on_press, on_release=on_release)
 
